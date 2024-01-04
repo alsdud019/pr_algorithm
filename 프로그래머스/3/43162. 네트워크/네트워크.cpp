4d5e666eca9b628714a33da11e095d6006374729
alsdud <83855203+alsdud019@users.noreply.github.com>
2024-01-04 21:14:22 +0900

    [level 3] Title: 네트워크, Time: 0.05 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<vector<int>> arr;
+int DAT[200];
+
+void dfs(int level){
+    
+    for (int i=0; i<arr[level].size(); i++){
+        if (DAT[i]==1)continue;
+        if (i!=level && arr[level][i]==1){
+            
+            DAT[i]=1;
+            dfs(i);
+        }
+    }
+    
+    
+} 
+
+int solution(int n, vector<vector<int>> computers) {
+    int answer = 0;
+    arr=computers;
+    
+    for (int i=0; i<n; i++){
+        if (DAT[i]!=1){
+            DAT[i]=1;
+            dfs(i);
+            answer++;
+        }
+        
+    }
+    
+    //cout<<cnt<<endl;
+    
+    return answer;
+}
