090a1fed1986761681c3ad053d9c6794753d0ff8
MinYeong Kim <83855203+alsdud019@users.noreply.github.com>
2024-07-30 11:41:15 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 4.12 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    int answer = 0;
+    int arr[11]={0};
+    for (int i=0; i<numbers.size(); i++){
+        arr[numbers[i]]=1;
+    }
+    for (int i=0; i<10; i++){
+        if(!arr[i]){
+            answer+=i;
+        }
+    }
+    
+    return answer;
+}
