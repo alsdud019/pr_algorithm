ed62267fa240407157744e837998f8116088ca39
MinYeong Kim <83855203+alsdud019@users.noreply.github.com>
2024-07-30 12:22:07 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string phone_number) {
+    string answer = "";
+    for (int i=phone_number.size()-5; i>-1; i--){
+        phone_number[i]='*';
+    }
+    return phone_number;
+}
