18aa2b0d75346982d0d15e31bb44bbdf334d4841
MinYeong Kim <83855203+alsdud019@users.noreply.github.com>
2024-08-09 15:35:49 +0900

    [level 1] Title: 콜라 문제, Time: 0.03 ms, Memory: 3.69 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
@@ -7,15 +7,9 @@ int solution(int a, int b, int n) {
     int answer = 0;
     int remain=0;
     while (n>=a){
-        if (n%a==0){
-            answer+=(n/a)*b;
-            n=(n/a)*b;
-        }
-        else {
-            // remain+=n%a;
-            answer+=(n/a)*b;
-            n=(n/a)*b+(n%a);
-        }
+        answer+=(n/a)*b;
+        n=(n/a)*b+(n%a);
+
         
     }
         
