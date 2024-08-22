c5b4ea366ec79ecfa4989e9311d1a5ebdb09587a
MinYeong Kim <83855203+alsdud019@users.noreply.github.com>
2024-08-22 12:46:35 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.04 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<vector<int>> board, vector<int> moves) {
+    int answer = 0;
+    int n=board.size();
+    
+    vector<int> vec={-1};
+    
+    for (int move: moves){
+        int cnt=0;
+        for (int i=0; i<n; i++){
+            int new_toy=board[i][move-1];
+            
+            if (cnt==1) break;
+            
+            if (new_toy!=0){
+                //vec 추가
+                if (vec.back()==new_toy){
+                    answer++;
+                    vec.pop_back();
+                }
+                else{
+                    vec.push_back(new_toy);
+                    
+                }
+                board[i][move-1]=0;
+                cnt++;
+            }
+            else continue;
+        }
+    }
+    
+    
+    return answer*2;
+}
