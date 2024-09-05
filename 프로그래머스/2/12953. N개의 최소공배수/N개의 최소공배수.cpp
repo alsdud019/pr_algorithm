eeda9196faecf8c6abe9ad6fb9b9c4c1ff08a7b6
MinYeong Kim <83855203+alsdud019@users.noreply.github.com>
2024-09-05 12:33:48 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b){
+    int MAX=max(a,b);
+    int MIN=min(a,b);
+    
+    while (MAX % MIN !=0){
+        int remain= MAX % MIN;
+        MAX=MIN;
+        MIN=remain;
+    }
+    return MIN;
+}
+
+
+
+int solution(vector<int> arr) {
+    int answer = 0;
+    
+    int now=arr[0];
+    for (int i=1; i<arr.size(); i++){
+        now=now * arr[i] /gcd(now, arr[i]);
+    }
+    
+
+    
+    return now;
+}
