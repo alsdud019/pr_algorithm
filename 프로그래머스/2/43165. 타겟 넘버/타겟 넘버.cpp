3c774d3277924faa9b7c23d3a4412da6624a19c1
MinYeong Kim <83855203+alsdud019@users.noreply.github.com>
2024-09-13 09:03:00 +0900

    [level 2] Title: 타겟 넘버, Time: 69.24 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -3,51 +3,24 @@
 #include <iostream>
 
 using namespace std;
-char op[2]={'+','-'};
 
-int levelNum;
-char used[21];
-int cnt;
-int targetNum;
+int answer = 0;
 
-void dfs(int level, vector <int> numbers) {
-
-    if (level > levelNum) {
-        return; 
+void dfs(vector<int> numbers, int target, int sum, int level){
+    if (level==numbers.size()){
+        if (target==sum) answer++;
+        return ;
     }
+    
+    dfs(numbers, target, sum+numbers[level],level+1);
+    dfs(numbers, target, sum-numbers[level],level+1);
 
-    if (level == levelNum) {
-        int sum=0;
-        for (int i = 0; i < levelNum; i++) {
-            if (used[i] == '+') {
-                sum += numbers[i];
-            }
-            else if (used[i] == '-') {
-                sum -= numbers[i];
-            }
-            //cout << used[i] <<numbers[i];
-        }
-        if (sum == targetNum) {
-            cnt++;
-        }
-        //cout << sum << endl;
-        //cout << endl;
-        return;
-    }
-
-    for (int i = 0; i < 2; i++) {
-
-        used[level] = op[i];
-        dfs(level + 1,numbers);
-        used[level] = '0';
-
-    }
 }
 
 int solution(vector<int> numbers, int target) {
-    //int answer = 0;
-    levelNum=numbers.size();
-    targetNum=target;
-    dfs(0,numbers);
-    return cnt;
+    
+
+    dfs(numbers,target,0, 0);
+    
+    return answer;
 }
