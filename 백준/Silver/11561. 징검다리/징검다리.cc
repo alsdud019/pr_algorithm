489521adf31c0f112ed101f56f9b3e5d340e6d01
MinYeong Kim <83855203+alsdud019@users.noreply.github.com>
2024-10-29 15:01:02 +0900

    [Silver III] Title: 징검다리, Time: 288 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11561. 징검다리/징검다리.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int T;
+using ll = unsigned long long;
+
+void cross_bridge(ll n) {
+	ll left = 1;
+	ll right = n;
+	ll answer = -1e9;
+
+	while (left <= right) {
+		ll mid = (left + right) / 2;
+
+		if (mid * (mid + 1) <= 2 * n) {
+			left = mid + 1;
+			answer = max(mid, answer);
+		}
+		else right = mid - 1;
+	}
+	cout << answer << '\n';
+
+}
+
+int main() {
+	cin >> T;
+
+	while (T--) {
+		ll N;
+		cin >> N;
+
+		cross_bridge(N);
+
+	}
+
+	return 0;
+}
