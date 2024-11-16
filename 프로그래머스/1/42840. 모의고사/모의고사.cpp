a02e95699e53b458668b045c8dc77c755929ef0c
MinYeong Kim <83855203+alsdud019@users.noreply.github.com>
2024-11-16 22:23:54 +0900

    [level 1] Title: 모의고사, Time: 0.08 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -4,44 +4,52 @@
 
 using namespace std;
 
-vector <int> a={1,2,3,4,5};
-vector <int> b={2,1,2,3,2,4,2,5};
-vector <int> c={3,3,1,1,2,2,4,4,5,5};
-
-int DAT[3];
-int max_idx;
-int MAX=0;
+int first[]={1,2,3,4,5};
+int second[]={2,1,2,3,2,4,2,5};
+int third[]={3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
 
 
 vector<int> solution(vector<int> answers) {
     vector<int> answer;
+    int mans[3]={0,};
+    
     for (int i=0; i<answers.size(); i++){
-        int a_mok=i%a.size();
-        if (answers[i]==a[a_mok]){
-            DAT[0]++;
+        int a=i%5;
+        if (answers[i]==first[a]){
+            mans[0]++;
         }
-        int b_mok=i%b.size();
-        if (answers[i]==b[b_mok]){
-            DAT[1]++;
+        
+        int b=i%8;
+        if (answers[i]==second[b]){
+            mans[1]++;
         }
-        int c_mok=i%c.size();
-        if (answers[i]==c[c_mok]){
-            DAT[2]++;
+        
+        int c=i%10;
+        if (answers[i]==third[c]){
+            mans[2]++;   
         }
+        
     }
-    for (int j=0; j<3; j++){
-        if (MAX<DAT[j]){
+
+    // for (int i=0; i<3; i++){
+    //     cout<<mans[i]<<" ";
+    // }
+    answer.push_back(1);
+    for (int i=1; i<3; i++){
+        int num=answer.back()-1;
+        
+        if (mans[num]<mans[i]){
             answer.clear();
-            answer.push_back(j+1);
-            MAX=DAT[j];
+            answer.push_back(i+1);
         }
-        else if(MAX==DAT[j]){
-            answer.push_back(j+1);
+        else if (mans[num]==mans[i]){
+            answer.push_back(i+1);
         }
+        
     }
     
-    
+
     
     return answer;
 }
