f5827df0dade4a3276e7d26f0a8ef7c279548b50
MinYeong Kim <83855203+alsdud019@users.noreply.github.com>
2024-11-29 13:11:09 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.05 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.cpp
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.cpp
@@ -1,76 +1,71 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
 string solution(string new_id) {
     string answer = "";
     
-    string no="~!@#$%^&*()=+[{]}:?,<>/";
-    
-    
+    string ban="~!@#$%^&*()=+[{]}:?,<>/";
     
     for (int i=0; i<new_id.size(); i++){
-        //1단계 소문자치환
-        if ('A'<=new_id[i] && new_id[i] <='Z') new_id[i]+=32;
+        //1단계
+        if ('A'<= new_id[i] && new_id[i]<='Z'){
+            new_id[i]=new_id[i]+32;
+        }
         
-        //2단계 사용가능 문자만
-        if (find(no.begin(), no.end(),new_id[i])!=no.end()) {
+        //2단계
+        if (ban.find(new_id[i]) != string::npos){
             new_id.erase(i,1);
             i--;
-        }       
+        }
+        
     }
     
     for (int i=0; i<new_id.size(); i++){
-        if(new_id[i]=='.') {
-            //첫번째일때 
-            if(i==0) 
-            {
+        if (new_id[i]=='.'){
+            //첫번째 위치하면
+            if (i==0){
                 new_id.erase(i,1);
-                i--;}
-            //연속일때
+                i--;
+            }
+            
+            //연달아 나오면
             else if (new_id[i+1]=='.'){
                 new_id.erase(i+1,1);
                 i--;
             }
-            //마지막일때
-            else if(i==new_id.size()-1){
+            //마지막에 위치하면
+            else if (i==new_id.size()-1){
                 new_id.erase(i,1);
                 i--;
             }
-            
-        }   
-        
-    }    
-    
-    
-    //5단계
-    if(new_id.size()==0) new_id='a';
-    
-    
-    //6단계
-    if (new_id.size()>15) 
-    {
-        new_id=new_id.substr(0,15);
-        
-        if (new_id[new_id.size()-1]=='.') new_id=new_id.substr(0,14);
+        }
     }
     
-    //7단계
-    if (new_id.size()<3) 
-    {
+    //5단계
+    if (new_id.size()<1){
+        new_id="a";
+    }
+    
+    if (new_id.size()>15){
+        new_id=new_id.substr(0,15);
         
-        while (new_id.size()<3){
-            
+        if (new_id[new_id.size()-1]=='.'){
+            new_id.erase(new_id.size()-1,1);
+        }
+    }
+    
+    if (new_id.size()<3){
+        while(new_id.size()!=3){
             new_id+=new_id[new_id.size()-1];
         }
     }
     
     
-    cout<<new_id<<endl;
+    answer=new_id;
     
     
-    return new_id;
+    return answer;
 }
