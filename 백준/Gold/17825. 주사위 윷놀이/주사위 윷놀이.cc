34025f8c5eff2ee61b0bb5aefbe4c4af57584984
MinYeong Kim <83855203+alsdud019@users.noreply.github.com>
2025-01-31 12:20:40 +0900

    [Gold II] Title: 주사위 윷놀이, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17825. 주사위 윷놀이/주사위 윷놀이.cc
+++ 백준/Gold/17825. 주사위 윷놀이/주사위 윷놀이.cc
@@ -1,53 +1,52 @@
 #include <iostream>
 
 using namespace std;
-int map[34];
-int turn[34];
-int horse[4] = {0,};
+
+int game[11];
+int MAP[34];
+int dir[34];
+
+int horse[4] = { 0, };
 int check[34];
-
-int dices[11];
-
 int answer = -1e9;
 
-
 void init() {
-
-	for (int i = 1; i < 21; i++) {
-		map[i] = 2 * i;
+	for (int i = 1; i <= 20; i++) {
+		MAP[i] = i * 2;
 	}
-	map[22] = 13; map[23] = 16; map[24] = 19;
-	map[25] = 25; map[26] = 30; map[27] = 35;
-	map[29] = 22; map[30] = 24;
-	map[31] = 28; map[32] = 27; map[33] = 26;
+	MAP[22] = 13; MAP[23] = 16; MAP[24] = 19;
+	MAP[25] = 25; MAP[26] = 30; MAP[27] = 35;
+	MAP[28] = 22; MAP[29] = 24;
+	MAP[30] = 28; MAP[31] = 27; MAP[32] = 26;
 
-	turn[5] = 22; turn[10] = 29; turn[15] = 31;
+	dir[5] = 22; dir[10] = 28; dir[15] = 30;
+	dir[29] = 25; dir[32] = 25; dir[27] = 20;
 
 
 }
 
 void dfs(int level, int sum) {
+
 	if (level == 10) {
 		answer = max(answer, sum);
 		return;
 	}
 
 	for (int i = 0; i < 4; i++) {
-		int move=dices[level];
-		int current = horse[i];
-		int next = current; //다음 이동: 현재 위치에서 시작
+		int cur = horse[i];
+		int next = cur;
+		int move = game[level];
 
-		//갈림길에서 방향 바꿔주기 
-		if (turn[current] > 0) {
-			next = turn[current];
+		if (dir[next] > 0) {
+			next = dir[next];
 			move--;
 		}
-		
+
 		while (move--) {
-			if (next == 30) {
+			if (next == 29) {
 				next = 25;
 			}
-			else if (next == 33) {
+			else if (next == 32) {
 				next = 25;
 			}
 			else if (next == 27) {
@@ -56,23 +55,23 @@ void dfs(int level, int sum) {
 			else if (next == 21) {
 				next = 21;
 			}
-			else next += 1;
+			else next++;
 		}
-		
-		if (next != 21 && check[next]) continue; //다음 옮길 장소가 도착장소가 아닌데 다른 말이 존재한다면 
 
-		check[current] = 0; //말이 있는 곳 해제하기
-		check[next] = 1; //말이 이동할 곳으로 체크하기 
+		if (check[next] && next != 21) continue;
+
 		horse[i] = next;
+		check[cur] = 0;
+		check[next] = 1;
 
-		dfs(level + 1, sum + map[next]);
+		dfs(level + 1, sum+MAP[next]);
 
-		check[current] = 1;
+		horse[i] = cur;
+		check[cur] = 1;
 		check[next] = 0;
-		horse[i] = current;
-
 
 	}
+
 }
 
 int main() {
@@ -80,10 +79,10 @@ int main() {
 	init();
 
 	for (int i = 0; i < 10; i++) {
-		cin >> dices[i];
+		cin >> game[i];
 	}
 
-	dfs(0,0);
+	dfs(0, 0);
 
 	cout << answer;
 
