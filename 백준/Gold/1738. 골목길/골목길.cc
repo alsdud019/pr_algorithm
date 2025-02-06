fb0b6772cd7d6cb7879eee3e1aa018a3898d2788
MinYeong Kim <83855203+alsdud019@users.noreply.github.com>
2025-02-06 14:18:58 +0900

    [Gold I] Title: 골목길, Time: 8 ms, Memory: 2164 KB -BaekjoonHub

--- 백준/Gold/1738. 골목길/골목길.cc
+++ 백준/Gold/1738. 골목길/골목길.cc
@@ -1,103 +1,106 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <queue>
 
 using namespace std;
+int n, m;
 
-int N, M;
 struct Node {
 	int to;
 	int cost;
 };
 
 vector<Node> vec[101];
+int D[101];
+int track[101];
 vector<int> rev[101];
-int dist[101];
-int answer[101];
 int able[101] = {0,};
 
-void check_bfs() {
+void bfs(int now) {
 	queue<int> q;
-	q.push(N);
+	q.push(now);
+	able[now] = 1;
 
-	while(!q.empty()) {
+	while (!q.empty()) {
+
 		int now = q.front(); q.pop();
 
 		for (int i = 0; i < rev[now].size(); i++) {
 			int next = rev[now][i];
-
 			if (able[next]) continue;
+
 			able[next] = 1;
 			q.push(next);
 		}
-
 	}
+
+	
 }
 
 int main() {
+	cin >> n >> m;
 
-	cin >> N >> M;
-
-	for (int i = 0; i < M; i++) {
-		int from, to, cost;
-		cin >> from >> to >> cost;
-		vec[from].push_back({ to, cost });
+	for (int i = 0; i < m; i++) {
+		int from, to, w;
+		cin >> from >> to >> w;
+		vec[from].push_back({ to, w });
 		rev[to].push_back(from);
+
 	}
 
-	check_bfs();
+	bfs(n);
 
-	for (int i = 1; i <= N; i++) {
-		dist[i] = -1e9;
+	for (int i = 1; i <= n; i++) {
+		D[i] = -1e9;
 	}
 
-	dist[1] = 0;
+	D[1] = 0;
 
-	for (int i = 0; i < N; i++) {
+	//n-1번 반복
+	for (int i = 1; i <= n - 1; i++) {
 
-		for (int j = 1; j <= N; j++) {
+		for (int j = 1; j <= n; j++) {
 			for (int k = 0; k < vec[j].size(); k++) {
-				int next = vec[j][k].to;
-				int cost = vec[j][k].cost;
+				int now = j;
+				int next = vec[now][k].to;
+				int cost = vec[now][k].cost;
 
-				if (dist[next] < dist[j] + cost) {
-					dist[next] = dist[j] + cost;
-					//cout << "j=" << j << " next=" << next << endl;
-					answer[next] = j;
-				}
+				if (D[now] + cost <= D[next]) continue;
+				D[next] = D[now] + cost;
+				//cout <<now << " " << next << endl;
+				track[next] = now;
 			}
 		}
 	}
 
-	for (int j = 1; j <= N; j++) {
+	//음수사이클 확인 
+	for (int j = 1; j <= n; j++) {
 		for (int k = 0; k < vec[j].size(); k++) {
-			int next = vec[j][k].to;
-			int cost = vec[j][k].cost;
+			int now = j;
+			int next = vec[now][k].to;
+			int cost = vec[now][k].cost;
 
-			if (dist[next] < dist[j] + cost) {
-				if (able[next]) {
-					cout << -1 << endl;
-					return 0;
-				}
-				
+			if (D[now] + cost <= D[next]) continue;
+			if (able[next]) {
+				cout << -1 << endl;
+				return 0;
 			}
 		}
 	}
 
-	vector<int> ans;
-	
-	int now = N;
-	while(1) {
-		ans.push_back(now);
-		if (now == 1) break;
-		now = answer[now];
+	int ago = n;
+	vector<int> answer;
+
+	while (1) {
+		answer.push_back(ago);
+		if (ago == 1) break;
+		ago = track[ago];
 	}
 
-	for (int i = ans.size() - 1; i >= 0; i--) {
-		cout << ans[i] << " ";
+
+	for (int i = answer.size() - 1; i >= 0; i--) {
+		cout << answer[i] << " ";
 	}
-	
 
 	return 0;
 }
