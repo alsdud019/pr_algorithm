27bc393114063d4920d37585e8b0496437c73159
MinYeong Kim <83855203+alsdud019@users.noreply.github.com>
2025-02-26 16:10:59 +0900

    [level 2] Title: N-Queen, Time: 73.20 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/12952. N－Queen/N－Queen.cpp
@@ -0,0 +1,60 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+int visited[15];
+vector<int> vec;
+int answer = 0;
+
+bool check(int now){
+    bool flag=true;
+    int now_level=vec.size();
+    
+    for (int i=0; i<vec.size(); i++){
+
+        if (abs(now-vec[i]) == abs(i-now_level)){
+            flag=false;
+            break;
+        }
+        if (!flag) return false;
+    }
+    if (flag) return true;
+    else return false;
+            
+}
+
+void dfs(int N, int level){
+    
+    if (level==N){
+        // for (auto v:vec){
+        //     cout<<v<<" ";
+        // }
+        // cout<<endl;
+        answer++;
+    }
+    
+    for (int i=0; i<N; i++){
+        if (visited[i]) continue;
+        
+        if (check(i)){
+            visited[i]=1;
+            vec.push_back(i);
+            dfs(N, level+1);
+            visited[i]=0;
+            vec.pop_back();
+        }
+
+        
+    }
+    
+}
+
+int solution(int n) {
+    
+    
+    dfs(n,0);
+    
+    return answer;
+}
