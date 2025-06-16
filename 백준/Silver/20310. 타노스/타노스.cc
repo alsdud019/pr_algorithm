1a88616a224c53cba307049aaa9a291b1aeeda05
MinYeong Kim <83855203+alsdud019@users.noreply.github.com>
2025-06-16 13:09:29 +0900

    [Silver III] Title: 타노스, Time: 0 ms, Memory: 2028 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.cc
+++ 백준/Silver/20310. 타노스/타노스.cc
@@ -2,10 +2,13 @@
 
 using namespace std;
 
+int check[501] = {0,};
+
 int main() {
 
 	string str;
 	cin >> str;
+
 
 	int zero = 0;
 	int one = 0;
@@ -15,12 +18,27 @@ int main() {
 		else if (str[i] == '1') one++;
 	}
 
-	string answer;
-	for (int i = 0; i < zero / 2; i++) {
-		answer += '0';
+	int erase_zero = 0;
+	for (int i = str.size() - 1; i >= 0; i--) {
+		if (erase_zero == zero / 2) break;
+		if (str[i] == '0') {
+			check[i] = 1;
+			erase_zero++;
+		}
 	}
-	for (int i = 0; i < one / 2; i++) {
-		answer += '1';
+
+	int erase_one = 0;
+	for (int i = 0; i<= str.size() - 1; i++) {
+		if (erase_one == one / 2) break;
+		if (str[i] == '1') {
+			check[i] = 1;
+			erase_one++;
+		}
+	}
+
+	string answer;
+	for (int i = 0; i < str.size(); i++) {
+		if (check[i] != 1) answer += str[i];
 	}
 
 	cout << answer;
