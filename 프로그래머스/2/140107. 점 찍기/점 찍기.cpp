be7d7b99168249a2ee6db7209314b10a3d71af13
MinYeong Kim <83855203+alsdud019@users.noreply.github.com>
2025-09-17 21:25:24 +0900

    [level 2] Title: 점 찍기, Time: 14.81 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+long long solution(int k, int d) {
+    long long answer = 0;
+    
+    for (int i=0; i<=d; i+=k){
+        long long y=sqrt((long long)d*d-(long long)i*i);
+        // cout<<y<<" ";
+        answer+=y/k+1;
+    }
+    
+    return answer;
+}
