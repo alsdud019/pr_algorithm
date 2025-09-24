56cda0543b9e6bbb1a5c908b30687cd0aed134db
MinYeong Kim <83855203+alsdud019@users.noreply.github.com>
2025-09-24 09:55:49 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 196.37 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.cpp
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.cpp
@@ -6,7 +6,6 @@
 using namespace std;
 
 vector<int> vec;
-vector<pair<int, long long>> ans={{0,0}};
 priority_queue<pair<int, long long>> pq;
 
 void find(vector<vector<int>> users, vector<int> emoticons){
@@ -32,21 +31,12 @@ void find(vector<vector<int>> users, vector<int> emoticons){
         else total+=sum;
     }
     
-    if (plus!=0 || total!=0) {
-        pq.push({plus, total});
-        // cout<<plus<<" "<<total<<'\n';
-    }
-    
-    
+    if (plus!=0 || total!=0) pq.push({plus, total}); 
 }
 
 void recur(int n, int level, vector<vector<int>> users, vector<int> emoticons){
     
     if (level==n){
-        // for (auto a:vec){
-        //     cout<<a<<" ";
-        // }
-        // cout<<'\n';
         find(users,emoticons);
         return ;
     }
@@ -63,7 +53,7 @@ vector<int> solution(vector<vector<int>> users, vector<int> emoticons) {
     vector<int> answer;
     
     recur(emoticons.size(),0,users, emoticons);
-    // cout<<pq.top().first<<" "<<pq.top().second;
+
     answer.push_back(pq.top().first);
     answer.push_back(pq.top().second);
     return answer;
